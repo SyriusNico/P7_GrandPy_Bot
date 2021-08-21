c61b55cfe32b5e6f711c7c62a92a46540540ad89
Nicolas <pnschlei@gmail.com>
2021-08-21 10:17:07 +0200

    write test

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,30 +1,10 @@
-import pytest
-from flask import Flask, render_template
+import requests
 
-def test_index_route():
-	app = Flask(__name__)
-	client = app.test_client()
-	url = "/"
+from botapp import app as app
 
-	@app.route("/")
-	def test_index():
-		return "test application route"
+def test_index():
+	with app.test_client() as c:
+		response = c.get('/')
+		assert response.status_code == 200
 
-	response = client.get(url)
-
-	assert response.status_code == 200
-
-
-	def test_hello(client):
-		response = client.get('/hello')
-		assert response.data == b'Hello, World!'
-
-def test_result_route():
-	app = Flask(__name__)
-	client = app.test_client()
-
-	@app.route("/")
-	def test_result():
-		req = client.post('something')
-		assert req.status_code == 200
 
