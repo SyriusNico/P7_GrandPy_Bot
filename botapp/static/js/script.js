de1eef148c46312765f60c84cd400b0f7322dcbe
Nicolas <pnschlei@gmail.com>
2021-08-22 22:03:42 +0200

    modified

--- botapp/static/js/script.js
+++ botapp/static/js/script.js
@@ -20,12 +20,7 @@ $("#query--form").submit(function(event) {
 	// Stop form from submitting normally
 	event.preventDefault();
  
-	// // Get some values from elements on the page:
-	// var $form = $(this),
-	// 	term = $form.find("input[name='question']").val(),
-	// 	url = $form.attr("action");
 
-	// var term = $("input[type=text][name=question]").val();
 
 	$.ajax({
 		data : $('#questionInput'),
@@ -45,21 +40,3 @@ $("#query--form").submit(function(event) {
 		});
 	});
  
-	// // Send the data using post
-	// var posting = $.post(url, {question: term});
- 
-	// // Put the results in a element
-	// posting.done(function(data) {
-	//   var text = data.query;
-	//   var address = data.label[0];
-	//   var lat = data.label[1].lat;
-	//   var lng = data.label[1].lng;
-	//   initMap(lat,lng);
-	//   $(".result").append($(".picture_papy"));
-	//   $("#papy").fadeOut(600).fadeIn(600);
-	//   $(".result").append("<h1>Voici l'adresse que tu cherchais : </h1>", address);  
-	//   $(".result").append("<h1>Est-ce que tu savais que ...</h1>", text);
-	//   $(".result").append($("<br><a class='link'>Pour plus d'informations</a><br>"));
-	//   $(".link").attr({href:'https://fr.wikipedia.org/wiki/' + data.title, target: '_blank'});
-
-	// });
